0648d4c2c7b3563462221963aad5a083bc4ae514
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-25 17:19:15 +1100

    chokidar

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 // this line above makes it so this file is executed via node rather than directly
 
-console.log('hello world');
+import chokidar from 'chokidar';
+
+chokidar.watch('.')
+  .on('add', () => console.log('FILE ADDED'))
+  .on('change', () => console.log('FILE CHANGED'))
+  .on('unlink', () => console.log('FILE HAS BEEN REMOVED'));
