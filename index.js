5a3c54c3aa338dcc7263031166d05eab17a99ffd
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-25 18:10:10 +1100

    caporal

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 // this line above makes it so this file is executed via node rather than directly
 
-import chokidar from 'chokidar';
+
 import debounce from 'lodash.debounce';
+import chokidar from 'chokidar';
+import program from 'caporal';
 
-const start = debounce(() => {
-  console.log('starting users program');
-}, 1000);
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action((args) => {
+    console.log(args);
+  });
 
-chokidar.watch('.')
-  .on('add', start)
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE HAS BEEN REMOVED'));
+  program.parse(process.argv);
+
+// const start = debounce(() => {
+//   console.log('starting users program');
+// }, 1000);
+
+// chokidar.watch('.')
+//   .on('add', start)
+//   .on('change', () => console.log('FILE CHANGED'))
+//   .on('unlink', () => console.log('FILE HAS BEEN REMOVED'));
